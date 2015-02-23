da5ce5622e779ea1c1a6bb9e7478d326c00c6031
pan0va <pan0va@users.noreply.github.com>
2015-02-23 03:26:04 +0300

    old assignment no longer needed

--- plot1.R
+++
@@ -1,7 +0,0 @@
-data<- read.table ("household_power_consumption.txt", header = TRUE, nrows=2075260, sep = ";", na.strings = "?")
-data<- subset(data, data$Date %in% c("1/2/2007","2/2/2007"))
-data$DateTime<- strptime(paste(data$Date,data$Time), format = "%d/%m/%Y %H:%M:%S" )
-data<- na.omit(data)
-dev.copy(png,'plot1.png', width = 480, height = 480)
-hist (data$Global_active_power, col = "red", ylim = c(0,1200), main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
-dev.off()
